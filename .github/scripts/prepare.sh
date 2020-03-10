710aedfe228f669be16d5f26caf77d7be5240672
Felix Marezki <felix.marezki@exceet.de>
2020-03-10 19:08:26 +0100

    fix: ookla

--- .github/scripts/prepare.sh
+++ .github/scripts/prepare.sh
@@ -5,4 +5,6 @@ whoami
 curl https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-x86_64-linux.tgz -o speedtest.tgz
 tar -xf speedtest.tgz
 
+ls -la
+
 mv ookla-speedtest-1.0.0-x86_64-linux/speedtest /usr/bin/speedtest
